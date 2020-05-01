74bd43eaa988cdf3f5f202b4ec10c2a6966597f2
Naima <nbensassi@hotmail.com>
2020-05-01 14:10:08 -0400

    add comment on files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
+    // port 3360
     port: 3306,
 
     // Your username
